151bf9ee92e0a76f31a4760e835e90582df8449f
Jay <jaydeshmukh8551@gmail.com>
2021-03-10 14:10:11 +0530

    initial attempt to add graphiql component

---
+++ src/graphiql.js
@@ -0,0 +1,117 @@
+import React, { useRef, useEffect, useState } from 'react';
+import GraphiQL from 'graphiql';
+import PropTypes from 'prop-types';
+import { buildSchema } from 'graphql';
+import GraphiQLExplorer from 'graphiql-explorer';
+
+const TykGraphiQL = (props) => {
+  const {
+    fetcher,
+    schema,
+    ideName = 'TykGraphiQL',
+    query: initialQuery = '{}',
+    showPlayground = true,
+    menu,
+  } = props;
+  const playgroundWrapperRef = useRef(null);
+  let playgroundRef = useRef(null);
+  const [explorerIsOpen, setExplorerIsOpen] = useState(false);
+  const [query, setQuery] = useState(initialQuery);
+  const [selectedMenu, setSelectedMenu] = useState(menu?.title);
+
+  useEffect(() => {
+    if (!playgroundWrapperRef.current) {
+      return;
+    }
+
+    playgroundWrapperRef.current.addEventListener('click', (e) => {
+      e.preventDefault();
+    });
+  }, [playgroundWrapperRef]);
+
+  const toggleExplorer = () => setExplorerIsOpen(!explorerIsOpen);
+
+  const handleMenuSelection = (item) => {
+    menu.onSelect(item);
+    setSelectedMenu(item?.label);
+  };
+
+  return (
+    <div
+      ref={playgroundWrapperRef}
+      className="graphiql-container"
+      style={{
+        visibility: showPlayground ? 'visible' : 'hidden',
+      }}
+    >
+      <GraphiQLExplorer
+        explorerIsOpen={explorerIsOpen}
+        onToggleExplorer={toggleExplorer}
+        onEdit={(q) => setQuery(q)}
+        schema={schema && buildSchema(schema)}
+        query={query}
+        onRunOperation={(operationName) =>
+          playgroundRef.handleRunQuery(operationName)
+        }
+      />
+      <GraphiQL
+        ref={(ref) => {
+          playgroundRef = ref;
+          return true;
+        }}
+        {...props}
+        schema={schema && buildSchema(schema)}
+        fetcher={fetcher}
+        query={query}
+      >
+        <GraphiQL.Logo key={'logo'}>{ideName}</GraphiQL.Logo>
+        <GraphiQL.Toolbar>
+          <GraphiQL.Button
+            key={'explorer'}
+            onClick={toggleExplorer}
+            label="Explorer"
+            title="Toggle Explorer"
+          />
+          <GraphiQL.Button
+            key={'Prettify'}
+            onClick={() => playgroundRef.handlePrettifyQuery()}
+            label="Prettify"
+            title="Prettify Query"
+          />
+          {menu?.items?.length && (
+            <GraphiQL.Menu
+              label={selectedMenu}
+              title={selectedMenu}
+              key={selectedMenu}
+            >
+              {menu.items.map((item) => (
+                <GraphiQL.MenuItem
+                  key={item.title}
+                  onSelect={() => handleMenuSelection(item)}
+                  title={item.title}
+                  label={item.label}
+                />
+              ))}
+            </GraphiQL.Menu>
+          )}
+        </GraphiQL.Toolbar>
+      </GraphiQL>
+    </div>
+  );
+};
+
+TykGraphiQL.propTypes = {
+  fetcher: PropTypes.func,
+  schema: PropTypes.string,
+  menu: PropTypes.shape({
+    label: PropTypes.string,
+    title: PropTypes.string,
+    onSelect: PropTypes.func,
+    items: PropTypes.array,
+  }),
+  ideName: PropTypes.string,
+  query: PropTypes.string,
+  showPlayground: PropTypes.bool,
+};
+
+export default TykGraphiQL;
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env) => {
+  let webpackConfig = {
+    entry: {
+      app: './src/index.js',
+    },
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'lib'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            'file-loader',
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                bypassOnDebug: true,
+                disable: true,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: [
+            'file-loader',
+          ],
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: './index.html',
+      }),
+    ],
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
+  };
+  if (!env) {
+    webpackConfig = {
+      ...webpackConfig,
+      mode: 'development',
+      devtool: 'inline-source-map',
+      devServer: {
+        contentBase: './dist',
+        open: true,
+        overlay: true,
+        stats: 'errors-only',
+        watchContentBase: true,
+      },
+    };
+  }
+  return webpackConfig;
+};
